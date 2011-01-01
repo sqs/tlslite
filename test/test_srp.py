2a21ac50369e6464802adbe6d12b8b3a8cd70e86
Quinn Slack <sqs@cs.stanford.edu>
2011-01-01 07:28:16 -0600

    move good_srp test into single function

--- test/test_srp.py
+++ test/test_srp.py
@@ -8,20 +8,21 @@ class TestSRP(unittest.TestCase):
         self.client = TestClient()
         self.server = TestServer()
     
-    def test_client_good_srp(self):                
-        with ServerThread(self.server, self.__server_good_srp):
+    def test_good_srp(self):
+        def server_good_srp():
+            verifierDB = VerifierDB()
+            verifierDB.create()
+            entry = VerifierDB.makeVerifier("test", "password", 1536)
+            verifierDB["test"] = entry        
+            sc = self.server.connect()
+            sc.handshakeServer(verifierDB=verifierDB)
+            sc.close()
+            sc.sock.close()
+
+        with ServerThread(self.server, server_good_srp):
             cc = self.client.connect()
             cc.handshakeClientSRP("test", "password")
             cc.close()
             cc.sock.close()
 
-    def __server_good_srp(self):
-        verifierDB = VerifierDB()
-        verifierDB.create()
-        entry = VerifierDB.makeVerifier("test", "password", 1536)
-        verifierDB["test"] = entry        
-        sc = self.server.connect()
-        sc.handshakeServer(verifierDB=verifierDB)
-        sc.close()
-        sc.sock.close()
 
